91a7dcf77300739cdfda952bf9d4047a4968c291
garfild <v.a.bogush@gmail.com>
2018-11-07 01:43:43 +0300

    p. #63

--- app/src/main/java/by/bossoftware/soft/geoquiz/QuizActivity.java
+++ app/src/main/java/by/bossoftware/soft/geoquiz/QuizActivity.java
@@ -2,7 +2,10 @@ package by.bossoftware.soft.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -15,6 +18,22 @@ public class QuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz);
 
         mTrueButton = (Button) findViewById(R.id.true_button);
-        mFalseButton= (Button) findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.correct_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(QuizActivity.this,
+                        R.string.incorrect_toast,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
